395a3c1a668310bfa4498e697ef20976a96e36f6
Tanishqsagar <tanishqsagar28@gmail.com>
2025-05-11 17:26:31 +0530

    changes in url

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ console.log("VITE_API_BASE_URL:", import.meta.env.VITE_API_BASE_URL);
 BASE_URL='https://xeno-crm-backend-77xi.onrender.com'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || BASE_URL,
+  baseURL: BASE_URL,
   // baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
   withCredentials: true,
 });
